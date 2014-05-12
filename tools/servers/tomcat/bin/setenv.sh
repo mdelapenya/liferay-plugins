2fa9b095cd4dac02d5b3546d0a82534efd27356c
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-05-12 11:57:11 +0200

    LPS-46140 Add JMX properties to setenv files

--- tools/servers/tomcat/bin/setenv.sh
+++ tools/servers/tomcat/bin/setenv.sh
@@ -1,1 +1,3 @@
-CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+JMX_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false
+
+CATALINA_OPTS="$CATALINA_OPTS $JMX_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
