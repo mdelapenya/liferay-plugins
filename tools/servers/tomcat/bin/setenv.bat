c76488381f4168eabfca85c5f55b8c5775857d64
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-05-12 12:07:02 +0200

    LPS-46140 Support edit JMX port by configuration

--- tools/servers/tomcat/bin/setenv.bat
+++ tools/servers/tomcat/bin/setenv.bat
@@ -6,6 +6,6 @@ if exist "%CATALINA_HOME%/jre@java.version@/win" (
 	set "JRE_HOME=%CATALINA_HOME%/jre@java.version@/win"
 )
 
-set "JMX_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+set "JMX_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=@arquillian.jmx.port@ -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 
 set "CATALINA_OPTS=%CATALINA_OPTS% %JMX_OPTS% -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
